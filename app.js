e760432ba95169e54e7f43a2cf9fa3159d0f9f6a
jslee6091 <jslee6091@naver.com>
2021-02-03 16:58:15 +0900

    exam-api

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000;
+// http://127.0.0.1:3000/
+pp.get('/service', (req, res)=> {res.send('The system is running [Status: ON'); req.status=on;});
+app.listen(port, () => console.log('example app listening on port 3000'))
